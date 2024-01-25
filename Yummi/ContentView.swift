a81868f751d4747b809ded80209ea8cfc33bd90a
Xan Culloden <Culloden.x@etoncollege.org.uk>
2024-01-25 13:11:05 +0000

    addded random

--- Yummi/ContentView.swift
+++ Yummi/ContentView.swift
@@ -8,23 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-
-    var currentIngredient = ingredient(name: "Apple ", quantity: 9, expiry: "12,03,2007", type: "froot")
+    let foods = ["apple","adi","anchovy","avaocado", "asparagus", "bannana", "baklava","beetroot","bangers abd mash","carrot","crayon","contuploupe", "cranberry","dumpling", "dragonfriut", "egg","eggplant","food", "fries","figs","grapefriut"," horse", "hamburger", "helva", "horseradish","iridescent powder","red 40", "gelatin","soy gop", "tren","acetate","ligament", "ABS","fungus","asbestos"]
+                @State private var currentIngredient = ingredient(name: "aple", quantity: 9, expiry: "12,03,2007", type: "froot")
     
     var body: some View {
         VStack {
-        
+
             Text(currentIngredient.displayStats())
-            
+            Button {
+                let randomFood = foods[Int.random(in: 1..<foods.count)]
+                let randomQuantity = Int.random(in: -2..<99)
+                let randomDate = String(" \(Int.random(in:1..<31)),\(Int.random(in:1...12)),\(Int.random(in:1993..<2026))")
+                currentIngredient = ingredient(name: randomFood, quantity: randomQuantity, expiry: randomDate, type: "froot")
+
+                print(randomFood)
+                print(currentIngredient.displayStats())
+            } label: {
+                Text("botton")
+            }
+
         }
         .padding()
-        Button("random ingredient") {
-            
-            let foods = ["apple","adi","anchovy","avaocado", "asparagus", "bannana", "baklava","beetroot","bangers abd mash","carrot","contuploupe", "cranberry","dumpling", "dragonfriut", "egg","eggplant","food", "fries","figs","grapefriut"," horse", "hamburger", "helva", ]
-            
-            currentIngredient[0] = foods[arc4random (in: 1..<foods.count) ]
-            
-        }
+//        Button("random ingredient") {
+//            let randomFood = foods[Int.random(in: 1..<foods.count)]
+//            var currentIngredient = ingredient(name: randomFood, quantity: 9, expiry: "12,03,2007", type: "froot")
+        //}
+        
+    
     }
     
 
